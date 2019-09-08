da1139b2fc2093595926e609b5aa38a8ff3dc121
OTRAY271 <OTRAY271@users.noreply.github.com>
2019-09-08 21:24:45 +0900

    fixed table

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,39 @@
 <template>
   <div id="app">
     <form name="form">
-    CollectionName
-    <input type="text" name="collectionName">
-    <br>
-    Token
-    <input type="password" name="token">
-    <br>
-    <br>
-    PerformanceName
-    <input type="text" name="performanceName">
-    <br>
-    SeatNumber
-    <input type="number" name="seatNumber" min="1">
-    <br>
-    UserId
-    <input type="text" name="userId">
-    <br>
-    <br>
-    Formation<br>
-    <textarea name="formation"></textarea>
+      CollectionName
+      <input type="text" name="collectionName" />
+      <br />Token
+      <input type="password" name="token" />
+      <br />
+      <br />PerformanceName
+      <input type="text" name="performanceName" />
+      <br />SeatNumber
+      <input type="number" name="seatNumber" min="0" />
+      <br />UserId
+      <input type="text" name="userId" />
+      <br />
+      <br />Formation
+      <br />
+      <textarea name="formation"></textarea>
     </form>
-    <br>
+    <br />
     <button id="makePerformance">makePerformance</button>
     <button id="setReservation">setReservation</button>
     <button id="getPerformance">getPerformance</button>
     <button id="getPerformanceList">getPerformanceList</button>
-    <br>
-    <br>
+    <br />
+    <br />
     <div id="table"></div>
     <seat-table :formation="formation" :seats="seats" />
   </div>
 </template>
 
 <script>
-import SeatTable from './components/SeatTable.vue'
+import SeatTable from "./components/SeatTable.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     SeatTable
   },
@@ -45,143 +41,143 @@ export default {
     return {
       formation: [],
       seats: {}
-    }
+    };
   },
   mounted() {
-    document.getElementById('getPerformance').addEventListener('click', () => {
-      var collectionName = document.form.collectionName.value
-      var token = document.form.token.value
-      var performanceName = document.form.performanceName.value
-      if(performanceName === ''){
-        alert("PerformanceNameを入力してください")
-      }else {
-        const xhr = new XMLHttpRequest()
-        xhr.open('POST', host + 'getPerformance')
+    document.getElementById("getPerformance").addEventListener("click", () => {
+      var collectionName = document.form.collectionName.value;
+      var token = document.form.token.value;
+      var performanceName = document.form.performanceName.value;
+      if (performanceName === "") {
+        alert("PerformanceNameを入力してください");
+      } else {
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", host + "getPerformance");
         xhr.onload = () => {
-          console.log(xhr.response)
-          const data = JSON.parse(xhr.response)
-          var formation = JSON.parse(data.formation)
-          this.formation = formation
-          this.seats = data.seats
-        }
-        xhr.send(JSON.stringify({
-          collectionName,
-          token,
-          performanceName
-        }))
+          console.log(xhr.response);
+          const data = JSON.parse(xhr.response);
+          var formation = JSON.parse(data.formation);
+          this.formation = formation;
+          this.seats = data.seats;
+        };
+        xhr.send(
+          JSON.stringify({
+            collectionName,
+            token,
+            performanceName
+          })
+        );
       }
-    })
+    });
   }
-}
+};
 
 // non-vue code below
 
-const host = 'https://asia-northeast1-easy-to-wait.cloudfunctions.net/'
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('makePerformance').addEventListener('click', () => {
-    var collectionName = document.form.collectionName.value
-    var token = document.form.token.value
-    var performanceName = document.form.performanceName.value
-    var formation = document.form.formation.value
-    if(performanceName === ''){
-      alert("PerformanceNameを入力してください")
-    }else {
-      const xhr = new XMLHttpRequest()
-      xhr.open('POST', host + 'makePerformance')
+const host = "https://asia-northeast1-easy-to-wait.cloudfunctions.net/";
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("makePerformance").addEventListener("click", () => {
+    var collectionName = document.form.collectionName.value;
+    var token = document.form.token.value;
+    var performanceName = document.form.performanceName.value;
+    var formation = document.form.formation.value;
+    if (performanceName === "") {
+      alert("PerformanceNameを入力してください");
+    } else {
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", host + "makePerformance");
       xhr.onload = () => {
-        console.log(xhr.response)
-      }
-      xhr.send(JSON.stringify({
-        collectionName,
-        token,
-        performanceName,
-        formation
-      }))
+        console.log(xhr.response);
+      };
+      xhr.send(
+        JSON.stringify({
+          collectionName,
+          token,
+          performanceName,
+          formation
+        })
+      );
     }
-  })
-  document.getElementById('setReservation').addEventListener('click', () => {
-    var collectionName = document.form.collectionName.value
-    var token = document.form.token.value
-    var performanceName = document.form.performanceName.value
-    var seatNumber_str = document.form.seatNumber.value
-    var userId = document.form.userId.value
-    var flag = false
-    if(performanceName === ''){
-      alert("PerformanceNameを入力してください")
-      flag = true
+  });
+  document.getElementById("setReservation").addEventListener("click", () => {
+    var collectionName = document.form.collectionName.value;
+    var token = document.form.token.value;
+    var performanceName = document.form.performanceName.value;
+    var seatNumber_str = document.form.seatNumber.value;
+    var userId = document.form.userId.value;
+    var flag = false;
+    if (performanceName === "") {
+      alert("PerformanceNameを入力してください");
+      flag = true;
     }
-    if(seatNumber_str === ''){
-      alert("SeatNumberを入力してください")
-      flag = true
-    }else if(isNaN(seatNumber_str)){
-      alert("無効なSeatNumberです")
-      flag = true
+    if (seatNumber_str === "") {
+      alert("SeatNumberを入力してください");
+      flag = true;
     }
-    if (userId === ''){
-      alert("UserIdを入力してください")
-      flag = true
-    }else if(isNaN(userId)){
-      alert("無効なUserIdです")
-      flag = true
+    if (userId === "") {
+      alert("UserIdを入力してください");
+      flag = true;
     }
-    var seatNumber = parseInt(seatNumber_str)
-    if(!flag){
-      const xhr = new XMLHttpRequest()
-      xhr.open('POST', host + 'setReservation')
+    var seatNumber = parseInt(seatNumber_str);
+    if (!flag) {
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", host + "setReservation");
       xhr.onload = () => {
-        console.log(xhr.response)
-      }
-      xhr.send(JSON.stringify({
-        collectionName,
-        token,
-        performanceName,
-        seatNumber,
-        userId
-      }))
+        console.log(xhr.response);
+      };
+      xhr.send(
+        JSON.stringify({
+          collectionName,
+          token,
+          performanceName,
+          seatNumber,
+          userId
+        })
+      );
     }
-  })
-  document.getElementById('getPerformanceList').addEventListener('click', () => {
-    var collectionName = document.form.collectionName.value
-    var token = document.form.token.value
-    const xhr = new XMLHttpRequest()
-    xhr.open('POST', host + 'getPerformanceList')
-    xhr.onload = () => {
-      console.log(xhr.response)
-    }
-    xhr.send(JSON.stringify({
-      collectionName,
-      token,
-    }))
-  })
-  const fillStyles = [
-    '#b2bec3',
-    '#e84393',
-    '#00cec9'
-  ]
-  function makeTable(data, seats, tableId){
-    var obj = document.getElementById(tableId)
-    obj.innerHTML = ''
-    var table = document.createElement("table")
-    table.style.width = (30*data[0].length).toString() + "px"
-    for(let i = 0; i < data.length; i++){
-        let row = table.insertRow(-1);
-        for(let j = 0; j < data[0].length; j++){
-            let cell = row.insertCell(-1)
-            const userId = seats[(i*data[0].length+j+1).toString()]
-            let colorIndex
-            if(typeof userId !== 'undefined'){
-              colorIndex = 0
-              cell.textContent = userId
-            }else {
-              colorIndex = data[i][j]
-              cell.textContent = "\u00a0"
-            }
-            cell.style.backgroundColor = fillStyles[colorIndex]
+  });
+  document
+    .getElementById("getPerformanceList")
+    .addEventListener("click", () => {
+      var collectionName = document.form.collectionName.value;
+      var token = document.form.token.value;
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", host + "getPerformanceList");
+      xhr.onload = () => {
+        console.log(xhr.response);
+      };
+      xhr.send(
+        JSON.stringify({
+          collectionName,
+          token
+        })
+      );
+    });
+  const fillStyles = ["#b2bec3", "#e84393", "#00cec9"];
+  function makeTable(data, seats, tableId) {
+    var obj = document.getElementById(tableId);
+    obj.innerHTML = "";
+    var table = document.createElement("table");
+    table.style.width = (30 * data[0].length).toString() + "px";
+    for (let i = 0; i < data.length; i++) {
+      let row = table.insertRow(-1);
+      for (let j = 0; j < data[0].length; j++) {
+        let cell = row.insertCell(-1);
+        const userId = seats[(i * data[0].length + j + 1).toString()];
+        let colorIndex;
+        if (typeof userId !== "undefined") {
+          colorIndex = 0;
+          cell.textContent = userId;
+        } else {
+          colorIndex = data[i][j];
+          cell.textContent = "\u00a0";
         }
+        cell.style.backgroundColor = fillStyles[colorIndex];
+      }
     }
-    obj.appendChild(table)
+    obj.appendChild(table);
   }
-})
+});
 </script>
 
 <style>
--- src/components/SeatTable.vue
+++ src/components/SeatTable.vue
@@ -1,46 +1,87 @@
 <template>
-    <table>
-        <tr v-for="(row, i) in formation">
-            <td v-for="(cell, j) in row" :style="{ backgroundColor: backgroundColor(cell, i * row.length + j) }">
-                {{ text(i * row.length + j ) }}
-            </td>
-        </tr>
-    </table>
+  <table @mouseleave="setActive(-1)">
+    <tr v-for="(row, i) in formation">
+      <td
+        class="cell"
+        v-for="(cell, j) in row"
+        :style="{ backgroundColor: backgroundColor(cell, i * row.length + j) , color: textColor(cell) }"
+        @mouseover="setActive(i * row.length + j)"
+      >
+        {{ i * row.length + j}}
+        <div class="fukidashi" v-if="isActive(i * row.length + j)">{{ text(i * row.length + j) }}</div>
+      </td>
+    </tr>
+  </table>
 </template>
 
 <script>
-const fillstyles = [
-    '#b2bec3',
-    '#e84393',
-    '#00cec9'
-]
+const fillstyles = ["#b2bec3", "#e84393", "#00cec9"];
+const textfillstyles = ["#808080", "#ffffff"];
 export default {
-    props: {
-        formation: Array,
-        seats: Object
+  props: {
+    formation: Array,
+    seats: Object
+  },
+  data: function() {
+    return {
+      activenum: -1
+    };
+  },
+  methods: {
+    text(index) {
+      const userId = this.seats[index];
+      if (userId) {
+        return userId;
+      }
+      return "";
     },
-    methods: {
-        text(index) {
-            const userId = this.seats[index]
-            if(userId) {
-                return userId
-            }
-            return ''
-        },
-        backgroundColor(cell, index){
-            const colorIndex = this.seats[index] ? 0 : cell
-            return fillstyles[colorIndex]
-        }
+    backgroundColor(cell, index) {
+      const colorIndex = this.seats[index] ? 0 : cell;
+      return fillstyles[colorIndex];
     },
-    watch: {
-
+    textColor(cell) {
+      return textfillstyles[cell === 0 ? 0 : 1];
+    },
+    isActive(index) {
+      return this.text(index) !== "" && index === this.activenum;
+    },
+    setActive(index) {
+      this.activenum = index;
     }
-}
+  },
+  watch: {}
+};
 </script>
 
 <style scoped>
 td {
-    width: 20px;
-    height: 20px;
+  width: 20px;
+  height: 20px;
+  position: relative;
+  text-align: center;
+}
+.fukidashi {
+  white-space: nowrap;
+  bottom: 30px;
+  left: 0px;
+  position: absolute;
+  padding: 5px;
+  border-radius: 2px;
+  background: #f563c4;
+  color: #fff;
+}
+.fukidashi:after {
+  position: absolute;
+  width: 0;
+  height: 0;
+  left: 0;
+  bottom: -19px;
+  margin-left: 1px;
+  border: solid transparent;
+  border-color: rgba(51, 204, 153, 0);
+  border-top-color: #f563c4;
+  border-width: 10px;
+  pointer-events: none;
+  content: " ";
 }
 </style>
