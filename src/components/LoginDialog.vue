9df2640a967b22801431193dbdbbb2609060e564
shundroid <shundroidk@gmail.com>
2020-05-20 08:40:37 +0900

    added login view (only view)

---
+++ src/components/LoginDialog.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-dialog v-model="show" persistent max-width="700">
+    <v-card>
+      <v-card-title>サイコの予約 ダッシュボード</v-card-title>
+      <v-card-text>
+        <v-select
+          :items="servers"
+          label="ログイン先" />
+        <v-radio-group v-model="loginType">
+          <v-radio label="v2でログイン(推奨)" value="v2" />
+          <v-radio label="v1でログイン" value="v1" />
+        </v-radio-group>
+        <v-btn block color="accent" v-if="loginType === 'v2'">
+          <v-icon>mdi-google</v-icon>
+          &nbsp;Google でログイン
+        </v-btn>
+        <v-text-field label="トークン" v-if="loginType === 'v1'" />
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: true,
+      servers: ['2019 2C', 'psycho-reservation-test'],
+      loginType: "v2"
+    }
+  }
+}
+</script>
